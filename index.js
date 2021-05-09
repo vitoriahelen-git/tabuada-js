ca05fa242a4af46d2de18b2cb524dbfeee647a4c
Vitoria Helen <vitoriahellen3004@gmail.com>
2021-05-09 18:50:26 -0300

    Tabuada final

---
+++ index.js
@@ -0,0 +1,8 @@
+let numero = prompt("Digite um número: "); 
+console.log ("Tabuado do numero: " + numero);
+
+let i = 0
+
+for(i; i <= 10; i++) {
+    console.log(numero + " x " + i + " = " + (numero * i))
+}
